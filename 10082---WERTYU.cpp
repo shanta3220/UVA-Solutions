d34efd8d841d12a8057f0057ba849627a4500b76
shanta <evilevil4991@yahoo.com>
2016-02-06 02:00:43 +0600

    Create 10082---WERTYU.cpp

---
+++ 10082---WERTYU.cpp
@@ -0,0 +1,106 @@
+#include<iostream>
+#include<stdio.h>
+using namespace std;
+int main()
+{
+    char b;
+     while(scanf("%c",&b)==1)
+    {
+
+        if (b=='1')
+            cout<<"`";
+        if (b=='2')
+            cout<<"1";
+        if (b=='3')
+            cout<<"2";
+        if (b=='4')
+            cout<<"3";
+        if (b=='5')
+            cout<<"4";
+        if (b=='6')
+            cout<<"5";
+        if (b=='7')
+            cout<<"6";
+        if (b=='8')
+            cout<<"7";
+        if (b=='9')
+            cout<<"8";
+        if (b=='0')
+            cout<<"9";
+        if (b=='-')
+            cout<<"0";
+        if (b=='=')
+            cout<<"-";
+        if (b=='W')
+            cout<<"Q";
+        if (b=='E')
+            cout<<"W";
+        if (b=='R')
+            cout<<"E";
+        if (b=='T')
+            cout<<"R";
+        if (b=='Y')
+            cout<<"T";
+        if (b=='U')
+            cout<<"Y";
+        if (b=='I')
+            cout<<"U";
+        if (b=='O')
+            cout<<"I";
+        if (b=='P')
+            cout<<"O";
+        if (b=='[')
+            cout<<"P";
+        if (b==']')
+            cout<<"[";
+        if (b=='\\')
+            cout<<"]";
+        if (b=='S')
+            cout<<"A";
+        if (b=='D')
+            cout<<"S";
+        if (b=='F')
+            cout<<"D";
+        if (b=='G')
+            cout<<"F";
+        if (b=='H')
+            cout<<"G";
+        if (b=='J')
+            cout<<"H";
+        if (b=='K')
+            cout<<"J";
+        if (b=='L')
+            cout<<"K";
+        if (b==';')
+            cout<<"L";
+        if (b=='\'')
+            cout<<";";
+        if (b=='X')
+            cout<<"Z";
+        if (b=='C')
+            cout<<"X";
+        if (b=='V')
+            cout<<"C";
+        if (b=='B')
+            cout<<"V";
+        if (b=='N')
+            cout<<"B";
+        if (b=='M')
+            cout<<"N";
+        if (b==',')
+            cout<<"M";
+        if (b=='.')
+            cout<<",";
+        if (b=='/')
+            cout<<".";
+        if (b==' ')
+            cout<<" ";
+        if (b=='\n')
+            cout<<"\n";
+
+
+
+    }
+    return 0;
+
+}
