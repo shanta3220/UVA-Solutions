f52333dcb2208349a902ff4825b057a5b39e6188
shanta <evilevil4991@yahoo.com>
2016-02-06 02:00:09 +0600

    Create 10071 Back to High School Physics.cpp

---
+++ 10071 Back to High School Physics.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<stdlib.h>
+using namespace std;
+int main()
+{
+    int t,s,v;
+    while(cin>>v>>t)
+    {
+       s=2*(t*v);
+       cout<<s<<"\n";
+    }
+    return 0;
+
+}
+
