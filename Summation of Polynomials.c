4017d16c7055f01d7b84af4197ba8675c07a6dfa
shanta <evilevil4991@yahoo.com>
2016-02-06 02:05:10 +0600

    Create Summation of Polynomials.c

---
+++ Summation of Polynomials.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+   int i,n,x,j=0;
+   long long s;
+   while(scanf("%d",&x)==1)
+       {
+            s=1;
+            s=s*x*(x+1)/2;
+            s=s*x*(x+1)/2;
+
+
+           printf("%lld\n",s);
+
+
+       }
+       return 0;
+
+   }
+
