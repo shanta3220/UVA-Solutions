938bd25cb7c81b41b790a59dd2a399ab75eadace
shanta <evilevil4991@yahoo.com>
2016-02-17 02:02:21 +0600

    Worked on first try :)

---
+++ 11150 - Cola.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main()
+{
+    int n=0,s=0;
+    while(cin>>n)
+    {
+        s=n+n/2;
+        cout<<s<<"\n";
+    }
+    return 0;
+}
