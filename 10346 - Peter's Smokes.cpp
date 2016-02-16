a1dc9e55e0bbd14a8f1c930d7e02b0fde3fd646a
shanta <evilevil4991@yahoo.com>
2016-02-17 03:23:26 +0600

    Create 10346 - Peter's Smokes.cpp

---
+++ 10346 - Peter's Smokes.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main()
+{
+    long long int n=0,k=0;
+    while(cin>>n>>k)
+    {
+
+        cout<<n+(n-1)/(k-1)<<"\n";
+
+    }
+    return 0;
+}
