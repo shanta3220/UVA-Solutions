63542563f1ef64ff99d5e8054239fd83cac62999
shanta <evilevil4991@yahoo.com>
2016-02-16 22:26:57 +0600

    Worked on first try :)

--- 10812 - Beat the Spread!.cpp
+++ 10812 - Beat the Spread!.cpp
@@ -8,16 +8,19 @@ int main()
     while(n--){
       long long int sum2=0,sum1=0;
       cin>>s>>d;
-      if(s<d)
+      if(s<d){
         cout<<"impossible\n";
+      }
       else{
         sum1 = s+d;
         sum2 = sum1/2;
-        if(s-sum2==sum2-d&&sum1%2==0)
-         cout<<sum2<<" "<<sum2-d<<"\n";
-        else cout<<"impossible\n";
+        if(s-sum2==sum2-d&&sum1%2==0){
+            cout<<sum2<<" "<<sum2-d<<"\n";
+        }
+        else{
+            cout<<"impossible\n";
+        }
       }
     }
     return 0;
 }
- 
