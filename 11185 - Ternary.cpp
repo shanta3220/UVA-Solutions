4bb92bd100e7d8b3d4d134701fcea336728e26b9
shanta <evilevil4991@yahoo.com>
2016-03-07 20:56:34 +0600

    Accepted

--- 11185 - Ternary.cpp
+++ 11185 - Ternary.cpp
@@ -4,28 +4,21 @@ using namespace std;
 
 int main()
 {
-    long int n,c=0,r=0, i=0, binary=0;
-    while((cin>>n)&&n!=-1)
-    {
-        long n2=n;
-        long n3=n;
-
-        while(n2!=0)
-        {
-            n2=n2/10;
-            c++;
+    int n;
+    while((cin>>n)&&n>-1){
+        int i=0,a[100];
+        if(n==0){
+            cout<<n;
         }
-
-          while(n3!=0&&i!=c-1){
-              r=n3%10;
-              binary=binary+r*pow(2,i);
-              n3=n3/10;
-              i++;
+        while(n!=0){
+            a[i]=n%3;
+            n=n/3;
+            i++;
         }
-
-        binary+=3;
-        cout<<binary;
-
-
+        for(int k=i-1; k>=0; k--){
+            cout<<a[k];
+        }
+        cout<<"\n";
     }
+    return 0;
 }
