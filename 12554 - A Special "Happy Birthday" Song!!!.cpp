1cec3fa1a9929958fde1e1a08a51c37f0b72ab34
shanta <evilevil4991@yahoo.com>
2016-09-27 01:20:41 +0600

    Update 12554 - A Special "Happy Birthday" Song!!!.cpp

--- 12554 - A Special "Happy Birthday" Song!!!.cpp
+++ 12554 - A Special "Happy Birthday" Song!!!.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main() {
 	char a[105][105],b[16][16]= {"Happy","birthday","to","you","Happy","birthday","to","you","Happy","birthday","to","Rujia","Happy","birthday","to","you"};
 	int t,c=0;
-	while(cin>>t){
+	cin>>t;
 	for(int i=0; i<t; i++)
 	    cin>>a[i];
 	    c=t/16;
@@ -13,7 +13,7 @@ int main() {
         c++;
      for(int i=0; i<c*16; i++)
 	 cout<<a[i%t]<<": "<<b[i%16]<<"\n";
-    } 
-	
+    
+
 	return 0;
 }
