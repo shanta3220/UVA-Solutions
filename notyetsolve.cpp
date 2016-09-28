eba1a930c59669c3873555931e1b87d79d5a633e
shanta <evilevil4991@yahoo.com>
2016-09-28 19:05:54 +0600

    Update notyetsolve.cpp

--- notyetsolve.cpp
+++ notyetsolve.cpp
@@ -1,28 +1,33 @@
 #include <iostream>
-#include<string>
+#include<string.h>
 using namespace std;
-
+char s[100000000],a[100000000];
 int main() {
-	string s,a;
+	
 	long long int i,j,k,n,l;
-	cin>>n>>s;
-	cin.ignore();
-	j=1;
-	while(j!=0){
-	 j=0;
-	for (i=0,k=0; i<s.length(); i++){
-	    if((s[i]=='1' && s[i+1]=='0') || (s[i]=='0' && s[i-1]=='1')){
-        j++;
+	cin>>n;
+	for (i=0; i<n; i++)
+	    cin>>s[i];
+	k=0;
+	for (i=0; i<n; i++){
+	    if((s[i]=='1' && s[i+1]=='0')){
+        i++;
+        continue;
+    }
+    else if((s[i+1]=='1'&&s[i]=='1' && s[i+2]=='0'&&s[i+3]=='0')){
+        i+=3;
         continue;
     }
     else{
             a[k]=s[i];
             k++;
+    } 
+    
+    a[k]='\0';
     }
-    l=a.length();
+    
+    
+	l=strlen(a);
     cout<<l<<"\n";
-	    
-	}
-	
 	return 0;
 }
