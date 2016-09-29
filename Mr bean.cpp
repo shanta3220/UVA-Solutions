4b9d918c6e1eb118d4cbfca6340036b9c68530c3
shanta <evilevil4991@yahoo.com>
2016-09-29 10:30:29 +0600

    Create Mr bean.cpp

---
+++ Mr bean.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t,l,w,h;
+	cin >> t;
+	while(t--){
+	   cin >> l >> w >> h;
+	   if( l <=20 && w <= 20 && h <= 20)
+	    cout << "Case "<< I++ << ": good\n";
+	    else cout << "Case " << I++ << ": bad\n";
+	}
+	return 0;
+}
