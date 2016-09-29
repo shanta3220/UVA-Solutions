348b019a6c4dd4a322a1b9d1711cb12ba466623f
shanta <evilevil4991@yahoo.com>
2016-09-29 12:11:41 +0600

    Accepted

--- 12372 - Packing for Holiday.cpp
+++ 12372 - Packing for Holiday.cpp
@@ -2,13 +2,13 @@
 using namespace std;
 
 int main() {
-	int t,l,w,h;
+	int t,l,w,h,i=1;
 	cin >> t;
 	while(t--){
 	   cin >> l >> w >> h;
 	   if( l <=20 && w <= 20 && h <= 20)
-	    cout << "Case "<< I++ << ": good\n";
-	    else cout << "Case " << I++ << ": bad\n";
+	    cout << "Case "<< i++ << ": good\n";
+	    else cout << "Case " << i++ << ": bad\n";
 	}
 	return 0;
 }
