fe77522507b714d98bc670c5872714ee63bb27bc
shanta <evilevil4991@yahoo.com>
2016-10-01 14:45:59 +0600

    Update 12015 - Google is Feeling Lucky.cpp

--- 12015 - Google is Feeling Lucky.cpp
+++ 12015 - Google is Feeling Lucky.cpp
@@ -3,22 +3,24 @@
 #include<string>
 using namespace std;
 
-int main() {
-	char a[100][100];
-	int t,s,b[10],j=1;
-	cin>>t;
+int main()
+{    
+    char a[100][100];
+	int t, s, b[100], j = 1;
+	cin >> t;
 	while(t--){
 	    int c=0;
-	    for(int i=0;i<10;i++){
+	    for(int i = 0; i < 10; i++){
 	    cin >> a[i] >> b[i];
-	       if(b[i]>=c)
-	       c=b[i];
+	       if(b[i] >= c){
+	           c=b[i];
+	       }
+	   
 	    }
-	  
-	     cout<<"Case #"<<j++<<": \n";
-	     for(int i=0;i<10;i++){
-	        if(b[i]==c)
-	      cout<< a[i]<<"\n";
+        cout << "Case #" << j++ << ":\n";
+	    for(int i = 0; i < 10; i++){
+	        if(b[i] == c)
+	            cout << a[i] << "\n";
 	       else continue;
 	    }
 	    
