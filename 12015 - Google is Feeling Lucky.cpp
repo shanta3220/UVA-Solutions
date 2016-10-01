9725f28fa2c89a39220fa8a5eed6efb6082f550b
shanta <evilevil4991@yahoo.com>
2016-10-01 14:32:28 +0600

    Create 12015 - Google is Feeling Lucky.cpp

---
+++ 12015 - Google is Feeling Lucky.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include<algorithm>
+#include<string>
+using namespace std;
+
+int main() {
+	char a[100][100];
+	int t,s,b[10],j=1;
+	cin>>t;
+	while(t--){
+	    int c=0;
+	    for(int i=0;i<10;i++){
+	    cin >> a[i] >> b[i];
+	       if(b[i]>=c)
+	       c=b[i];
+	    }
+	  
+	     cout<<"Case #"<<j++<<": \n";
+	     for(int i=0;i<10;i++){
+	        if(b[i]==c)
+	      cout<< a[i]<<"\n";
+	       else continue;
+	    }
+	    
+	}
+
+	return 0;
+}
