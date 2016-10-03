7b12d1937d1beaed1c1017ad24eda295bcde1721
shanta <evilevil4991@yahoo.com>
2016-10-03 22:24:21 +0600

    Update 10591 - Happy Number.cpp

--- 10591 - Happy Number.cpp
+++ 10591 - Happy Number.cpp
@@ -13,7 +13,7 @@ int main() {
 	        sum = 0;
 	        while( n != 0){
 	            sum += (n % 10) * (n % 10);
-	            n = n/10;
+	            n = n / 10;
 	            
 	        }
 	        n = sum;
