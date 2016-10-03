b7974bf3eeeec2006a3b85d6379cd40d4d3070af
shanta <evilevil4991@yahoo.com>
2016-10-03 22:20:57 +0600

    Accepted

--- 10591 - Happy Number.cpp
+++ 10591 - Happy Number.cpp
@@ -2,14 +2,14 @@
 using namespace std;
 
 int main() {
-	long long int n, n2, j = 1, t, sum, r;
+	long long int n, n2;
+	int j = 1, t, sum;
 	cin >> t;
 	while(t--){
 	    cin >> n2;
-	    
-        if(n2 < 10)  n = n2 * n2;
+	    if(n2 < 10)  n = n2 * n2;
         else n = n2;
-   
+       if(n >9){
 	    while (n > 9){
 	        sum = 0;
 	        while( n != 0){
@@ -19,9 +19,13 @@ int main() {
 	        }
 	        n = sum;
 	     }
-	     if(n == 1 )
+	      if(n == 1 )
 	           cout << "Case #" << j++ << ": " << n2 <<  " is a Happy number.\n";
 	        else cout << "Case #" << j++ << ": " << n2 << " is an Unhappy number.\n";  
+       }
+	    else if (n == 1 || n == 7 )
+	           cout << "Case #" << j++ << ": " << n2 <<  " is a Happy number.\n";
+	   else cout << "Case #" << j++ << ": " << n2 << " is an Unhappy number.\n";  
     }
 	return 0;
 }
