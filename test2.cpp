6079d7c33dc4859db2128af21f1b04397ea28481
shanta <evilevil4991@yahoo.com>
2016-10-03 22:16:26 +0600

    Update test2.cpp

--- test2.cpp
+++ test2.cpp
@@ -13,14 +13,13 @@ int main() {
 	    while (n > 9){
 	        sum = 0;
 	        while( n != 0){
-	            r = n % 10;
-	            sum += r * r;
+	            sum += (n % 10) * (n % 10);
 	            n = n/10;
 	            
 	        }
 	        n = sum;
 	     }
-	     if(n == 1 || n ==7)
+	     if(n == 1 )
 	           cout << "Case #" << j++ << ": " << n2 <<  " is a Happy number.\n";
 	        else cout << "Case #" << j++ << ": " << n2 << " is an Unhappy number.\n";  
     }
