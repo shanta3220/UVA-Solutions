49b52dece5af08929c9dc2e254a4e0b905d10fbb
shanta <evilevil4991@yahoo.com>
2016-10-06 03:39:08 +0600

    Accepted

--- 272 - TEX Quotes.cpp
+++ 272 - TEX Quotes.cpp
@@ -2,10 +2,11 @@
 #include <string.h>
 using namespace std;
 
+
 int main() {
 	string s;
-	while(getline(cin,s) && s!='EOF'){
-	    int fact = 1;
+	int fact = 1;
+	while(getline(cin,s)){
 	    for(int i = 0; i <s.size(); i++){
 	    if(s[i]=='"'){
 	        if(fact ==1){
@@ -24,4 +25,3 @@ int main() {
 	}
 	return 0;
 }
-
