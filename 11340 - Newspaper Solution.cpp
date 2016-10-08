7d66ace0ac6bfac7a59de243b320f8c3af32f14d
shanta <evilevil4991@yahoo.com>
2016-10-08 17:45:28 +0600

    Accepted

--- 11340 - Newspaper Solution.cpp
+++ 11340 - Newspaper Solution.cpp
@@ -1,22 +1,25 @@
 #include <iostream>
 #include <string>
-#include <cstdio>
+#include <iomanip>
 using namespace std;
 
 int main() {
-	long int t, n, m, b[200];
-	char a[200];
+	long int t, n, m; 
 	cin >> t;
 	while(t--){
 	    double sum =0;
 	    cin >> n;
 	    cin.ignore();
+	    char a[n];
+	    int b[n];
+	    
 	    for(int i = 1; i <= n; i++){
-	        cin >> a[i] >> b[i];
-	        
-	    }   
+            cin >> a[i] >> b[i];
+	    }    
+	      
 	    cin >> m;
 	    cin.ignore();
+	    
 	    string s;
 	    while(m--){
 	        getline(cin,s);
@@ -28,7 +31,8 @@ int main() {
 	        }
 	   
 	    }
-	   printf("%.2lf$\n",sum/100);
+	    
+	    cout << setprecision(2) << fixed << sum/100 << "$\n";
 	}
 	return 0;
 }
