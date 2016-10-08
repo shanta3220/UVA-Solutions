2b07a54ed52a7369d49638ae83247aff1f1c0161
shanta <evilevil4991@yahoo.com>
2016-10-08 17:21:50 +0600

    Create 11340 - Newspaper Solution.cpp

--- 11340 - Newspaper Solution.cpp
+++ 11340 - Newspaper Solution.cpp
@@ -8,25 +8,25 @@ int main() {
 	char a[200];
 	cin >> t;
 	while(t--){
+	    double sum =0;
 	    cin >> n;
-	    
+	    cin.ignore();
 	    for(int i = 1; i <= n; i++){
 	        cin >> a[i] >> b[i];
 	        
 	    }   
 	    cin >> m;
+	    cin.ignore();
 	    string s;
-	    double sum = 0;
-	    for(int j = 1;j<=m;j++){
+	    while(m--){
 	        getline(cin,s);
 	        for(int i = 1; i <= n; i++){
 	            for(int k = 0; k < s.size(); k++){
-    	        if(s[k]==a[i]) sum += b[i];
-    	       
-    	        } 
+    	        	if(s[k]==a[i]) sum += b[i];
+    	      	    } 
 	        }
 	   }
-	   printf("%.2lf$\n",sum/100);
+	   cout << sum/100 <<"$\n";
 	}
 	return 0;
 }
