43ff3e44e2c8b986945255bf0d181f32699e6818
shanta <evilevil4991@yahoo.com>
2016-10-09 21:20:37 +0600

    Accepted

--- 401 - Palindromes.cpp
+++ 401 - Palindromes.cpp
@@ -3,55 +3,58 @@
 using namespace std;
 
 bool isPalindrome(string input){
-	  int len = input.size();
+    int len = input.size();
     bool isPalindrome = true;
-	  for(int j = 0; j<len/2; j++){
-        if(input[j] != input[len - 1 -j]){
-          isPalindrome = false;	
-          break;
+    for(int j = 0; j<len/2; j++){
+	if(input[j] != input[len - 1 -j]){
+		isPalindrome = false;	
+		break;
 			
-	      }
+	}
     }
 	
-	return isPalindrome;
+    return isPalindrome;
 }
 bool isMirrored(string input){
-	  int len = input.size();
-	  int l =len -1;
+    int len = input.size();
+    int l =len -1;
     bool isPalindrome = true;
     for(int i = 0; i<len; i++){
-        if(input[i]=='B'||input[i]=='C'||input[i]=='D'||input[i]=='F'||input[i] =='G'||input[i]=='K'||input[i]=='N'||input[i]=='4'||input[i]=='6'||input[i]=='P'||input[i]=='Q' ||input[i]=='R'||input[i]=='7'||input[i]=='9'){
-            isPalindrome = false;
-            return isPalindrome;
+        if(input[i]=='B'||input[i]=='C'||input[i]=='D'||input[i]=='F'||input[i] =='G'||input[i]=='K'||input[i]=='N'
+	   ||input[i]=='4'||input[i]=='6'||input[i]=='P'||input[i]=='Q' ||input[i]=='R'||input[i]=='7'||input[i]=='9'){
+            	isPalindrome = false;
+            	return isPalindrome;
         }
-        if(input[i]==input[l-i]){if(input[i]=='Z'||input[i]=='5'||input[i]=='2'||input[i]=='3'||input[i]=='L'||input[i]=='J'||input[i]=='E'||input[i]=='S'){
-            isPalindrome = false;
-            return isPalindrome;
+        if(input[i]==input[l-i]){
+		if(input[i]=='Z'||input[i]=='5'||input[i]=='2'||input[i]=='3'
+		   ||input[i]=='L'||input[i]=='J'||input[i]=='E'||input[i]=='S'){
+            	     isPalindrome = false;
+            	     return isPalindrome;
             }
         }
         
         
     }
-	for(int i = 0; i<len/2; i++){
-	    if(input[i] != input[len - 1 -i]){
-		    if(input[i]=='2'&&input[len-1-i]=='S') isPalindrome = true;
-            else if(input[i]=='3'&&input[len-1-i]=='E') isPalindrome = true;
-            else if(input[i]=='L'&&input[len-1-i]=='J') isPalindrome = true;
-            else if(input[i]=='0'&&input[len-1-i]=='O') isPalindrome = true;
-            else if(input[i]=='5'&&input[len-1-i]=='Z') isPalindrome = true;
-            else if(input[i]=='S'&&input[len-1-i]=='2') isPalindrome = true;
-            else if(input[i]=='E'&&input[len-1-i]=='3') isPalindrome = true;
-            else if(input[i]=='J'&&input[len-1-i]=='L') isPalindrome = true;
-            else if(input[i]=='O'&&input[len-1-i]=='0') isPalindrome = true;
-            else if(input[i]=='Z'&&input[len-1-i]=='5') isPalindrome = true;
-    		else{
-    		    isPalindrome = false;	
-    			  break;
-    			
-    		}
+     for(int i = 0; i<len/2; i++){
+	  if(input[i] != input[len - 1 -i]){
+		 if(input[i]=='2'&&input[len-1-i]=='S') isPalindrome = true;
+		 else if(input[i]=='3'&&input[len-1-i]=='E') isPalindrome = true;
+		 else if(input[i]=='L'&&input[len-1-i]=='J') isPalindrome = true;
+		 else if(input[i]=='0'&&input[len-1-i]=='O') isPalindrome = true;
+		 else if(input[i]=='5'&&input[len-1-i]=='Z') isPalindrome = true;
+		 else if(input[i]=='S'&&input[len-1-i]=='2') isPalindrome = true;
+		 else if(input[i]=='E'&&input[len-1-i]=='3') isPalindrome = true;
+		 else if(input[i]=='J'&&input[len-1-i]=='L') isPalindrome = true;
+		 else if(input[i]=='O'&&input[len-1-i]=='0') isPalindrome = true;
+		 else if(input[i]=='Z'&&input[len-1-i]=='5') isPalindrome = true;
+		 else{
+			isPalindrome = false;	
+			break;
+
+		 }
 	    }
 	}
-	return isPalindrome;
+      return isPalindrome;
 }
 
 int main(){
