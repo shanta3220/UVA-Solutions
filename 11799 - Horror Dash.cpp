5fefcb801c76bcc527a8cb82409a526d6896a873
shanta <evilevil4991@yahoo.com>
2016-10-11 17:49:14 +0600

    Accepted

--- 11799 - Horror Dash.cpp
+++ 11799 - Horror Dash.cpp
@@ -5,7 +5,7 @@ int main()
 {   
     int t, num, n, j = 1; 
     cin >> t;
-    priority_queue<int> minspeedclown;
+    priority_queue <int> minspeedclown;
     while(t--){
         cin >> n;
         for(int i = 0; i <n; i++){
