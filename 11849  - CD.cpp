e7fedae476ff1aa629c198f91fb1c5e75e13b8dc
shanta <evilevil4991@yahoo.com>
2016-10-12 20:08:00 +0600

    Create 11849  - CD.cpp

---
+++ 11849  - CD.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <map>
+using namespace std;
+
+int main() {
+	long long m2, n2, m, n;
+	while(cin >> n >> m){
+	    if(m == 0 && n == 0) break;
+	    int c = 0;
+	    map <long, long> mapn;
+	    for(int i = 0; i < n; i++){
+	        cin >> n2;
+	        mapn[n2] = 1;
+	    }
+      
+	    for(int j = 0; j < m; j++){
+	        cin >> m2;
+	        if(mapn[m2] == 1)
+	           c++;
+	    }
+	    
+	    cout <<c<<"\n";
+  }         
+	
+	    
+	return 0;
+}
