03d9e7c1369fd63fd1313356aa46e6a1a3180b15
shanta <evilevil4991@yahoo.com>
2016-10-12 17:47:30 +0600

    Create test2.cpp

---
+++ test2.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <map>
+#include <cstring>
+using namespace std;
+
+int main() {
+	int t, n;
+	string cans, party;
+	cin >> t;
+	while(t--){
+	    cin >> n;
+	    while(getchar()!='\n');
+	    map <string, string> map1;
+	    map <string, int > map2;
+	    while(n--){
+	        getline(cin, cans);
+	        getline(cin, party);
+	        map1[cans] = party;
+	    }
+	   cin >> n;
+	   while(getchar()!='\n');
+	   while(n--){
+	       cin >> cans;
+	       map2[cans]+=1;
+	   }
+	   
+	   int max = -1, maxn = 0, c;
+	   string winner ="";
+	   map<string, string> :: iterator i;
+	   for(i = map1.begin(); i != map1.end(); i++){
+	       c = map2[i->first];
+	       if ( c > max){
+	         max = c;
+	         maxn = 0;
+	         winner = (*i).second;
+	       }
+	       else if (map2[i->first] == max) {
+				maxn++;
+            }
+	   }
+	    if (maxn != 1)
+            winner = "tie";
+        cout << winner << "\n";
+        if(t)
+           cout<<"\n";
+	   
+	   
+	}
+	return 0;
+}
